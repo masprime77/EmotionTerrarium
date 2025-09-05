cf2e54950a48bd911cccf58461e2d02b4f786f15
Mateo Arenas Salas <masprime77@gmail.com>
2025-09-05 17:45:06 +0200

    Final Test M1 -MA

--- firmware/main.py
+++ firmware/main.py
@@ -9,11 +9,12 @@ TEST_URL = "http://worldtimeapi.org/api/ip"  # usamos http plano
 
 def main():
     led = LedBuiltin()
-    wifi = WiFiService(config.SSID, config.PASSWORD, led=led)
+    wifi = WiFiService(config.WIFI_SSID, config.WIFI_PASSWORD, led=led)
 
     if wifi.connect(timeout_s=20):
         print("Connected. IP:", wifi.ifconfig()[0])
         data = http_get_json(TEST_URL, timeout=10)
+        print("Data:", data)
         tz = data.get("timezone", "?")
         print("Timezone:", tz)
     else:

0fd1cd30c38177c6a5de5e736dcd7adbb3a91879
Mateo Arenas Salas <masprime77@gmail.com>
2025-09-05 17:15:35 +0200

    Update

--- firmware/main.py
+++ firmware/main.py
@@ -8,17 +8,16 @@ def main():
     led = LEDBuiltin()
     wifi = WiFiService(config.WIFI_SSID, config.WIFI_PASSWORD, led = led)
 
-    print("[WiFi] Conectando a:", config.WIFI_SSID)
+    print("[WiFi] Connecting to:", config.WIFI_SSID)
     ok = wifi.connect(timeout_s=20)
     if not ok:
-        print("[WiFi] No se pudo conectar (timeout). Verifica SSID/clave o tipo de red.")
+        print("[WiFi] Error (timeout). Verify SSID or PASSWORD.")
     else:
-        print("[WiFi] Conectado. IFCONFIG:", wifi.ifconfig())
+        print("[WiFi] CONNECTED. IFCONFIG:", wifi.ifconfig())
 
-    # Bucle mínimo que vigila conexión
     while True:
         if not wifi.ensure_connected(timeout_s=10):
-            print("[WiFi] Desconectado. Reintentando...")
+            print("[WiFi] Disconnected. Retrying...")
         time.sleep(5)
 
 if __name__ == "__main__":
